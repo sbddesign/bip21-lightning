cc723d29c34fe83d3a9c9bb9f314863f59bf94b3
Stephen DeLorme <stephen@d.elor.me>
2022-03-24 11:44:07 -0400

    Update parameters

--- index.js
+++ index.js
@@ -3,16 +3,19 @@ const qrcode = require('qrcode')
 const { exec } = require('child_process')
 const args = require('minimist')(process.argv.slice(2))
 
-if(!args['btc'] || !args['amt'] || !args['description'] || !args['ln']) {
+if(!args['btc'] || !args['ln']) {
     console.log('Error: missing required args.');
 }
 else {
-    const uri = bip21.encode(args['btc'], {
-        amount: args['amt'],
-        label: args['name'] ? args['name'] + ': ' + args['description'] : args['description'],
-        message: args['description'],
+    let options = {
         lightning: args['ln']
-    })
+    }
+
+    if(args['amount']) options.amount = args['amount']
+    if(args['name']) options.label = args['name']
+    if(args['description']) options.message = args['description']
+    
+    const uri = bip21.encode(args['btc'], options)
     
     console.log('BIP21 Payment URI 👇\n\n' + uri + '\n\n')
 
